3b39092af4ec345a8e9299a0dfd5eae6e05b206c
Berkkan <berkkan.katirci@studium.uni-hamburg.de>
2024-11-02 17:29:13 +0100

    t

--- api/backend/main.py
+++ api/backend/main.py
@@ -39,7 +39,7 @@ def check_prerequisites():
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S', filename='./logs.log'
+    datefmt='%Y-%m-%d %H:%M:%S', filename='logs.log'
 )
 
 check_prerequisites()
